fb0711cacd9adfe22bd0b53dee69b5a281c0e2d6
Daniil Zulin <33616420+eistern@users.noreply.github.com>
2021-11-08 18:47:12 +0700

    Code style refactoring

--- src/clazz/base.clj
+++ src/clazz/base.clj
@@ -1,23 +1,23 @@
 (ns clazz.base
   [:require [clazz.shared :as shared]])
 
-(defn declare_class
-  ""
+(defn declare_class!
+  "Add entry to the class map. class_name will be used as the key, and it will be used in the `new_obj` function"
   [class_name vector_of_attributes & list_of_superclasses]
-  (shared/add_class class_name vector_of_attributes list_of_superclasses)
+  (shared/add_class! class_name vector_of_attributes list_of_superclasses)
   )
 
 (defn default_value
-  ""
+  "Factory method for the default value initialization"
   [value]
-  (fn DefaultGetter [] value)
+  (fn DefaultCreator [] value)
   )
 
 (defn new_obj
-  ""
+  "Instance new object of given class. class_name must exist as a key of the class map. See `declare_class!`"
   [class_name]
   (->>
-    ((shared/get_state_map) class_name)
+    ((shared/get_class_state_map) class_name)
     (vec)
     (reduce
       #(if
@@ -31,13 +31,13 @@
   )
 
 (defn get_value
-  ""
+  "Get value from the object by attribute name"
   [class_object attribute_name]
   (@class_object attribute_name)
   )
 
-(defn set_value
-  ""
+(defn set_value!
+  "Set the value of the object attribute"
   [class_object attribute_name attribute_value]
   (do
     (swap! class_object #(assoc %1 attribute_name attribute_value))
--- src/clazz/shared.clj
+++ src/clazz/shared.clj
@@ -1,11 +1,23 @@
 (ns clazz.shared)
 
 (let [class_state_map (atom {})]
-  (defn get_state_map []
-    @class_state_map)
-  (defn add_class [class_name vector_of_attributes & list_of_superclasses]
+
+  (defn get_class_state_map
+    "Get image of class state map. Changes to this object will not affect shared context"
+    []
+    @class_state_map
+    )
+
+  (defn add_class!
+    "Add new entry to the class state map. class_name should be the same as the class_name that will be passed to the new_obj method"
+    [class_name vector_of_attributes & list_of_superclasses]
     (swap! class_state_map #(assoc %1 class_name vector_of_attributes))
     )
-  (defn print_state_map []
-    (print @class_state_map))
+
+  (defn print_class_state_map
+    "Print current state of class map. Use only for debug"
+    []
+    (println @class_state_map)
+    )
+
   )
